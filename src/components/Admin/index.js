88bd2b7ad9b9596f48c84608a48b6e7d2c4d5a02
Ketan <ketantoo@gmail.com>
2020-06-20 20:50:11 +0530

    Admin Page

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,9 +1,32 @@
 import React from "react";
 
-const Admin = () => (
-  <div>
-    <h1>Admin Page </h1>
-  </div>
-);
+import { withFirebase } from "../Firebase";
 
-export default Admin;
+class Admin extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      users: {},
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    this.props.firebase.users().on("value", (snapshot) => {
+      this.setState({ users: snapshot.val(), loading: false });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Admin</h1>
+      </div>
+    );
+  }
+}
+
+export default withFirebase(Admin);
