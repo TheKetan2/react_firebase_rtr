e0a9f314546dab86687566003bae113b3073d6f9
Ketan <ketantoo@gmail.com>
2020-06-20 21:03:06 +0530

    Admin Page

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -12,6 +12,10 @@ class Admin extends React.Component {
     };
   }
 
+  componentWillUnmount() {
+    this.props.firebase.users().off();
+  }
+
   componentDidMount() {
     this.setState({ loading: true });
 
@@ -27,12 +31,33 @@ class Admin extends React.Component {
   }
 
   render() {
+    const { users, loading } = this.state;
     return (
       <div>
         <h1>Admin</h1>
+        {loading && <div>Loading...</div>}
+        <UserList users={users} />
       </div>
     );
   }
 }
+
+const UserList = ({ users }) => (
+  <ul>
+    {users.map((user) => (
+      <li key={user.uid}>
+        <span>
+          <strong>ID:</strong> {user.uid}
+        </span>
+        <span>
+          <strong>E-Mail:</strong> {user.email}
+        </span>
+        <span>
+          <strong>Username:</strong> {user.username}
+        </span>
+      </li>
+    ))}
+  </ul>
+);
 
 export default withFirebase(Admin);
